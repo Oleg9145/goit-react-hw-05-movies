75f7a815b7ca4222deb844105772674f2dbf1f2c
Oleg9145 <oleg.zinenko94@gmail.com>
2024-01-19 20:07:48 +0100

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Home } from './Home';
-import { Movies } from './Movies';
-import { MovieDetails } from './MovieDetails';
-import { Cast } from './Cast';
-import { Reviews } from './Reviews';
 import css from '../css/app.module.css';
+
+const Home = lazy(() =>
+  import('./Home').then(module => ({ default: module.Home }))
+);
+const Movies = lazy(() =>
+  import('./Movies').then(module => ({ default: module.Movies }))
+);
+const MovieDetails = lazy(() =>
+  import('./MovieDetails').then(module => ({ default: module.MovieDetails }))
+);
+const Cast = lazy(() =>
+  import('./Cast').then(module => ({ default: module.Cast }))
+);
+const Reviews = lazy(() =>
+  import('./Reviews').then(module => ({ default: module.Reviews }))
+);
+
 function App() {
   return (
     <Router basename="/oleg9145.github.io/goit-react-hw-05-movies">
@@ -15,18 +27,23 @@ function App() {
             <Link className={css.a} to="/">
               Home
             </Link>
-
-            <Link to="/movies/">Movies</Link>
+          </li>
+          <li>
+            <Link className={css.a} to="/movies/">
+              Movies
+            </Link>
           </li>
         </ul>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies/" element={<Movies />} />
-        <Route path="/movie/:movieId" element={<MovieDetails />} />
-        <Route path="/movies/:movieId/cast" element={<Cast />} />
-        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies/" element={<Movies />} />
+          <Route path="/movie/:movieId" element={<MovieDetails />} />
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
